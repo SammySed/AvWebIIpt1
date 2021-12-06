a1e012878d0de78e830e1aef56b706097d139fdd
SamuelssVieira <samuelvieira9643@hotmail.com>
2021-12-06 19:57:38 -0300

    Primeira Parte

---
+++ AvaliaçãoSistemaWebII/Models/Fornecedor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AvaliaçãoSistemaWebII.Models
+{
+    public class Fornecedor
+    {
+        public long FornecedorId { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string logradouro { get; set; }
+        public string Numero { get; set; }
+        public string Complemento { get; set; }
+        public string Bairro { get; set; }
+        public string cidade { get; set; }
+        public Produto produto { get; set; }
+    }
+}
---
+++ AvaliaçãoSistemaWebII/Models/Produto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AvaliaçãoSistemaWebII.Models
+{
+    public class Produto
+    {
+        public long ProdutoId { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }        
+    }
+}
